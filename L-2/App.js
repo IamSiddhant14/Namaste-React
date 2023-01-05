13a473746623216e4e01d3e0c60988700771181e
Siddhant Sharma <siddhantsharma669@gmail.com>
2023-01-05 16:14:21 +0530

    Added the file

---
+++ L-2/App.js
@@ -0,0 +1,40 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+const ele1 = React.createElement('h1', {
+        
+    id:"myId1",
+    className:"myClassName1",
+    style: {color: "red", backgroundColor: "blue"}
+
+}, 
+
+'Hello from REACT1' 
+
+);
+
+const ele2 = React.createElement('h1', {
+    
+    id:"myId2",
+    className:"myClassName2",
+    style: {color: "red", backgroundColor: "blue"}
+
+}, 
+
+'Hello from REACT2'
+
+);
+
+
+const ele3 = React.createElement('div', {     
+    id:"container",
+}, 
+
+[ele1 , ele2]
+
+);
+
+const root =ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(ele3);
